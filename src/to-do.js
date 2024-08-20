a89227637c77ec5efbc9664dadbd8c2c77619028
cierra <138401596+cdcutshaw@users.noreply.github.com>
2024-08-20 16:43:27 -0600

    create class for constructing todos

---
+++ src/to-do.js
@@ -0,0 +1,21 @@
+
+class todoItem {
+  constructor (title, description, dueDate, project, priority, completionStatus)  {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.project = project;
+    this.priority = priority;
+    this.completionStatus = completionStatus;
+  }
+};
+
+
+
+
+
+
+
+
+
+
