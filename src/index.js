ed26d7a1bf8c1f03e59d946f607fd6fd714cbfa9
cierra <138401596+cdcutshaw@users.noreply.github.com>
2024-08-20 16:37:20 -0600

    import project class and test that project logic works

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import "./styles.css";
+import projectController from "./project"
+
+
+const project = new projectController();
+//test project logic
+project.getProjects();
+project.clearProjects();
+project.getProjects();
+project.createProject('chores');
+project.createProject('errands');
+project.getProjects();
+project.deleteProject('errands');
+project.getProjects();
+project.updateProject('chores', 'housework')
+project.getProjects();
+
+
+
